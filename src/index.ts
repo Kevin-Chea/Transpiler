80a894e7da32f6ea3bba3a79448f6ecc463c29e7
Kevin Chea <kcl.chea@gmail.com>
2025-05-03 12:35:25 +0200

    [ADD] Mini complete run example

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import * as fs from 'fs';
+import { tokenize } from './tokenizer';
+import { parse } from './parser';
+import { interpret } from './interpreter';
 
 console.log('hello');
-const code = fs.readFileSync('examples/basic.tiny', 'utf-8');
+// const code = fs.readFileSync('examples/basic.tiny', 'utf-8');
+// const code = fs.readFileSync('examples/basicWithouthSpaces.tiny', 'utf-8');
+const code = fs.readFileSync('examples/severalVarDeclarations.tiny', 'utf-8');
+const tokens = tokenize(code);
+const ast = parse(tokens);
+const env = interpret(ast);
+console.log(env);
